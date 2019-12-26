04196cafecdf7901d21ead39f2cc8e1628135c6b
cesar mota <cesarmota@ymail.com>
2019-12-26 21:52:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -58,7 +58,7 @@ def reduce_to_total (source_array, starting_point=0)
   counter= 0 
 
   while counter < source_array.length do
-    new_array.push (starting_point + source_array[counter])
+    new_array.push (starting_point += source_array[counter])
     counter += 1 
   end
   new_array.last 
